95793d85ff5f9ffd64a37be4d80165cf546e6442
Vimal Kumar <vim22al@gmail.com>
2026-05-11 16:32:40 +0530

    chore: enforce strict sanity environment variable validation

--- src/sanity/env.ts
+++ src/sanity/env.ts
@@ -2,12 +2,12 @@ export const apiVersion =
   process.env.NEXT_PUBLIC_SANITY_API_VERSION || '2023-05-03'
 
 export const dataset = assertValue(
-  process.env.NEXT_PUBLIC_SANITY_DATASET || 'production',
+  process.env.NEXT_PUBLIC_SANITY_DATASET,
   'Missing environment variable: NEXT_PUBLIC_SANITY_DATASET'
 )
 
 export const projectId = assertValue(
-  process.env.NEXT_PUBLIC_SANITY_PROJECT_ID || 'missing-project-id',
+  process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
   'Missing environment variable: NEXT_PUBLIC_SANITY_PROJECT_ID'
 )
 
